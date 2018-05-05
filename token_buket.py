260315091f8ee9916a95d6eabdfc29d3564c68f0
Kalana Dhananjaya <Emny.incore@gmail.com>
2018-05-05 12:12:28 +0530

    test Multi thread and filling bucket

--- token_buket.py
+++ token_buket.py
@@ -1,6 +1,25 @@
 import time, threading
-def foo():
-    print(time.ctime())
-    threading.Timer(10, foo).start()
 
-foo()
+class bucket (threading.Thread):
+    
+    def __init__ (self, bucket_capacity,Filling_rate):
+        threading.Thread.__init__(self)
+        self.capacity=bucket_capacity
+        self.Filling_rate= Filling_rate
+        self.token=0
+
+    def run(self):
+        while(1):
+            time.sleep(2)
+            print(self.Filling_rate)
+        
+        
+    def __getTokenCount(self):
+        return token
+
+
+
+b2=bucket(100,5)
+b2.start()
+b1=bucket(100,4)
+b1.start()
